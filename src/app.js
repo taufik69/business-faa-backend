87ec1ee84a46a2a76eb682fe5e0e1703a198bc02
taufik69 <taufik35-2915@diu.edu.bd>
2026-04-05 09:47:17 +0600

    option removed add

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
